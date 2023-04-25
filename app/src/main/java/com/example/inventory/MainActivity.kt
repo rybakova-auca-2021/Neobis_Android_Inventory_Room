c53665595797e0d1164a96ea00598eca662ecedd
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-04-25 19:50:06 +0600

    Added navigation between fragments and bottom navigation

---
+++ app/src/main/java/com/example/inventory/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.inventory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.inventory.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mainFragment = MainFragment()
+        val archiveFragment = ArchiveFragment()
+
+        setCurrentFragment(mainFragment)
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener{
+            when(it.itemId) {
+                R.id.mainFragment-> setCurrentFragment(mainFragment)
+                R.id.archiveFragment-> setCurrentFragment(archiveFragment)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+}
